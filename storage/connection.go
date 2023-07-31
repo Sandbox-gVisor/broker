46137c0b43e34b0052a92d62fb3aea60ddbc12bf
Eduard <eduardvartazaryan1@gmail.com>
2023-07-31 11:37:10 +0700

    made redis flushing if client disconnected

--- storage/connection.go
+++ storage/connection.go
@@ -32,6 +32,10 @@ func (store *Storage) Init() {
 	}
 }
 
+func (store *Storage) FlushRedis() {
+	store.RedisClient.Del(store.ctx, "length", "pulled")
+}
+
 func (store *Storage) Close() {
 	if err := store.RedisClient.Close(); err != nil {
 		log.Fatal(err)
